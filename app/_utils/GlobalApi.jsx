e5c9209fae713eba53b069e3c1decca10533a7d0
yehabeshalij <73356173+yehabeshalij@users.noreply.github.com>
2024-10-06 03:26:01 +0300

    GlobalApi.jsx

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -3,7 +3,7 @@ const { default: axios } = require("axios");
 const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY;
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:1337/api",
+  baseURL: "https://ba-admin-strapi.onrender.com",
   headers: {
     Authorization: `Bearer ${API_KEY}`,
   },
